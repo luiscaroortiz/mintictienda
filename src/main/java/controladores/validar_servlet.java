7a2d17ff8d415f4722a88c18b7edbdd89b0267c2
luiscaroortiz <luiscaroortiz@gmail.com>
2021-09-27 19:04:46 -0500

    Conexion a base de datos

--- src/main/java/controladores/validar_servlet.java
+++ src/main/java/controladores/validar_servlet.java
@@ -1,11 +1,16 @@
 package controladores;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import controladores.Json;
+import controladores.Usuarios;
 
 /**
  * Servlet implementation class validar_servlet
@@ -21,15 +26,44 @@ public class validar_servlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    public void validarUsuarios(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	try {
+		ArrayList<Usuarios> lista = Json.getJSON();
+		request.setAttribute("lista", lista);
+		String usua = request.getParameter("txtusuario");
+		String pass = request.getParameter("txtpassword");
+		int respuesta =0;
+		for (Usuarios usuario:lista){
+			if (usuario.getUsuario().equals(usua) && usuario.getPassword().equals(pass)) {
+			    request.setAttribute("usuario", usuario);
+			    request.getRequestDispatcher("controlador?menu=Principal").forward(request, response);
+			    respuesta =1;
+			}
+					
+		}
+			
+		if (respuesta==0) {
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			System.out.println("No se encontraron datos");
+		}
+			
+	} catch (Exception e) {
+			// TODO: handle exception
+		e.printStackTrace();
+		}
+    	}	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-
 		String accion = request.getParameter("accion");
+
+		if (accion.equals("Ingresar")) {
+			this.validarUsuarios(request, response);
+		}
+
+/*		String accion = request.getParameter("accion");
         if (accion.equalsIgnoreCase("Ingresar")) {
 
 			String usua = request.getParameter("txtusuario");
@@ -49,7 +83,7 @@ public class validar_servlet extends HttpServlet {
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 
 		}
-
+*/
 
 	}
 
